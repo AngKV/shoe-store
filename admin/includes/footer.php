32750b9ee60b10e421cf13e90d2aa961179b8b81
AngKV <kyanh160601@gmail.com>
2023-05-12 16:15:50 +0700

    fix

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -4,7 +4,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Bug Shop 2020</span>
+            <span>Copyright &copy; Bug Shop 2023</span>
           </div>
         </div>
       </footer>
